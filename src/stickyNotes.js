d8646c122f7c7fad021ac200583c4a8ea8a873d7
Josh 『 0xDream 』 <100609687+JoshWeb3@users.noreply.github.com>
2023-04-10 09:17:35 -0700

    Initial commit

---
+++ src/stickyNotes.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function StickyNotes() {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="note">
+          0x..2FAB <br />
+          $100
+          <br />
+          Have a good day!
+        </div>
+      </div>
+    </div>
+  );
+}
